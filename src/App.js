67dbd602a2627b27618f8145c6ffed57dcb04652
viji033 <vijay240901@gmail.com>
2025-04-21 12:30:44 +0530

    updated footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/pages/Header';
 import About from './components/pages/About';
 import Resume from './components/pages/Resume';
@@ -8,14 +8,13 @@ import Portfolio from './components/pages/Portfolio';
 import Blog from './components/pages/Blog';
 import Contact from './components/pages/Contact';
 import Footer from './components/pages/Footer';
-import Hire from './components/pages/Hire'; // ✅ Import Hire
+import Hire from './components/pages/Hire';
 
 function AppContent() {
   const location = useLocation();
 
   return (
     <div id="wrapper">
-      {/* Main Content */}
       <Routes>
         <Route path="/" element={<Header />} />
         <Route path="/about" element={<About />} />
@@ -24,10 +23,9 @@ function AppContent() {
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/hire" element={<Hire />} /> {/* ✅ Add Hire route */}
+        <Route path="/hire" element={<Hire />} />
       </Routes>
 
-      {/* Footer appears only if not on Home Page */}
       {location.pathname !== '/' && <Footer />}
     </div>
   );
@@ -35,7 +33,7 @@ function AppContent() {
 
 function App() {
   return (
-    <Router basename="/portfolio">
+    <Router>
       <AppContent />
     </Router>
   );
