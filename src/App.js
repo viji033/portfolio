508826a182590e24cf761461a82915f959098f9d
viji033 <vijay240901@gmail.com>
2025-04-22 11:02:26 +0530

    json change

--- src/App.js
+++ src/App.js
@@ -1,3 +1,48 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+// import Header from './components/pages/Header';
+// import About from './components/pages/About';
+// import Resume from './components/pages/Resume';
+// import Services from './components/pages/Services';
+// import Portfolio from './components/pages/Portfolio';
+// import Blog from './components/pages/Blog';
+// import Contact from './components/pages/Contact';
+// import Footer from './components/pages/Footer';
+// import Hire from './components/pages/Hire';
+
+// function AppContent() {
+//   const location = useLocation();
+
+//   return (
+//     <div id="wrapper">
+//       <Routes>
+//         <Route path="/" element={<Header />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/resume" element={<Resume />} />
+//         <Route path="/services" element={<Services />} />
+//         <Route path="/portfolio" element={<Portfolio />} />
+//         <Route path="/blog" element={<Blog />} />
+//         <Route path="/contact" element={<Contact />} />
+//         <Route path="/hire" element={<Hire />} />
+//       </Routes>
+
+//       {location.pathname !== '/' && <Footer />}
+//     </div>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <Router basename="/portfolio">
+//       <AppContent />
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/pages/Header';
@@ -26,6 +71,7 @@ function AppContent() {
         <Route path="/hire" element={<Hire />} />
       </Routes>
 
+      {/* Show footer on all pages except home */}
       {location.pathname !== '/' && <Footer />}
     </div>
   );
@@ -33,10 +79,11 @@ function AppContent() {
 
 function App() {
   return (
-    <Router basename="/portfolio">
+    <Router>
       <AppContent />
     </Router>
   );
 }
 
 export default App;
+
